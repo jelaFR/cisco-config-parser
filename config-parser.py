1848020b888f698ac7a959668d9de884c9be8775
Jean-Eudes LAYRISSE <je.layrisse@spf-solutions.fr>
2021-01-07 15:02:26 +0100

    Minor fixes

--- config-parser.py
+++ config-parser.py
@@ -1,3 +1,8 @@
+import os
+import xlsxwriter
+from ciscoconfparse import CiscoConfParse
+from tqdm import tqdm
+
 def search_file_in_directory(directory,filter=""):
     """
     This function search all files inside a directory with a specific item in filter
@@ -5,7 +10,6 @@ def search_file_in_directory(directory,filter=""):
     :param filter: (optionnal) Return only files containing this value
     :return: List containing files with full path
     """
-    import os
     file_list = list()
     for root,subdir,files in os.walk(directory):
         for file in files:
@@ -23,7 +27,6 @@ def sh_run_to_dict(filename, output_dictionary, platform="ios"):
     :param platform: "ios" or "Catalyst" for regular Catalyst IOS; "nxos" or "nexus" for Cisco Nexus platform
     :return: A dictionary containing list of switches and ifaces parameters
     """
-    from ciscoconfparse import CiscoConfParse
     #Check platform type if specified
     if (platform.lower() == "catalyst") or (platform.lower() == "ios"):
         parse = CiscoConfParse (filename, factory=True, syntax='ios')
@@ -44,9 +47,10 @@ def sh_run_to_dict(filename, output_dictionary, platform="ios"):
         global_voice_vlan = global_voice_vlan[0].text.split()[2]
     # Get interfaces list and parse it
     all_ifaces = parse.find_objects ("^interface")
+
     for iface_param in all_ifaces:
         # Populate ifaces params in dictionnary for each iface
-        if iface_param.is_ethernet_intf or iface_param.is_portchannel:
+        if iface_param.is_ethernet_intf or iface_param.re_search_children ("channel-group"):
 
             # Get interface name and initiate dictionary
             iface_name = iface_param.text.replace ("interface ", "")
@@ -63,7 +67,7 @@ def sh_run_to_dict(filename, output_dictionary, platform="ios"):
             # Get interface description
             iface_desc = iface_param.re_search_children ("description")
             if iface_desc:
-                iface_desc = iface_desc[0].text.replace (" description ", "")  # VAR iface_desc
+                iface_desc = iface_desc[0].text.replace ("  description ", "")  # VAR iface_desc
             else:
                 iface_desc = ''  # VAR iface_desc (iface without description)
             output_dictionary[hostname][iface_name]["description"] = iface_desc
@@ -76,7 +80,7 @@ def sh_run_to_dict(filename, output_dictionary, platform="ios"):
                 output_dictionary[hostname][iface_name]["authentication"] = ''
 
             # Check if iface is part of port-channel
-            if iface_param.in_portchannel:
+            if iface_param.re_search_children ("channel-group"):
                 iface_etherchannel = iface_param.portchannel_number
                 output_dictionary[hostname][iface_name]["etherchannel_id"] = iface_etherchannel
             else:
@@ -85,7 +89,7 @@ def sh_run_to_dict(filename, output_dictionary, platform="ios"):
             # Collect access vlan configured
             iface_access_vlan = iface_param.re_search_children ("switchport access vlan")
             if iface_access_vlan:
-                iface_access_vlan = iface_access_vlan[0].text.replace (" switchport access vlan ", "")
+                iface_access_vlan = iface_access_vlan[0].text.replace ("  switchport access vlan ", "")
             else:
                 iface_access_vlan = ''
             output_dictionary[hostname][iface_name]["access_vlan"] = iface_access_vlan
@@ -93,7 +97,7 @@ def sh_run_to_dict(filename, output_dictionary, platform="ios"):
             # Collect voice vlan configured (if any)
             iface_voice_vlan = iface_param.re_search_children ("switchport voice vlan")
             if iface_voice_vlan:
-                iface_voice_vlan = iface_voice_vlan[0].text.replace (" switchport voice vlan ", "")
+                iface_voice_vlan = iface_voice_vlan[0].text.replace ("  switchport voice vlan ", "")
             elif global_voice_vlan:
                 iface_voice_vlan = global_voice_vlan
             else:
@@ -103,12 +107,12 @@ def sh_run_to_dict(filename, output_dictionary, platform="ios"):
             # Collect allowed vlans trunk informations
             iface_trunk_vlan = iface_param.re_search_children ("switchport trunk allowed vlan")
             if iface_trunk_vlan:
-                iface_trunk_vlan = iface_trunk_vlan[0].text.replace (" switchport trunk allowed vlan ", "")
+                iface_trunk_vlan = iface_trunk_vlan[0].text.replace ("  switchport trunk allowed vlan ", "")
                 if iface_param.re_search_children ("switchport trunk allowed vlan add"):
                     len_iface_trunk_add = len(iface_param.re_search_children ("switchport trunk allowed vlan add"))
                     for index in range(len_iface_trunk_add):
                         iface_trunk_vlan_add = iface_param.re_search_children ("switchport trunk allowed vlan add")[index]\
-                            .text.replace(" switchport trunk allowed vlan add ","")
+                            .text.replace("  switchport trunk allowed vlan add ","")
                         iface_trunk_vlan = iface_trunk_vlan+","+iface_trunk_vlan_add
             else:
                 iface_trunk_vlan = ''
@@ -118,17 +122,35 @@ def sh_run_to_dict(filename, output_dictionary, platform="ios"):
             # Collect native vlan trunk informations
             iface_trunk_native = iface_param.re_search_children ("switchport trunk native vlan")
             if iface_trunk_native:
-                iface_trunk_native = iface_trunk_native[0].text.replace (' switchport trunk native vlan ', '')
+                iface_trunk_native = iface_trunk_native[0].text.replace ('  switchport trunk native vlan ', '')
             else:
                 iface_trunk_native = ''
             output_dictionary[hostname][iface_name]["trunk_native"] = iface_trunk_native
 
+            # Collect speed informations
+            iface_speed = iface_param.re_search_children ("speed")
+            if iface_speed:
+                iface_speed = iface_speed[0].text.replace ('  speed ', '')
+            else:
+                iface_speed = 'auto'
+            output_dictionary[hostname][iface_name]["iface_speed"] = iface_speed
+
+            # Collect duplex informations
+            iface_duplex = iface_param.re_search_children ("duplex")
+            if iface_duplex:
+                iface_duplex = iface_duplex[0].text.replace ('  duplex ', '')
+            else:
+                iface_duplex = 'auto'
+            output_dictionary[hostname][iface_name]["iface_duplex"] = iface_duplex
+
             # Collect full previous configuration for rollback
-            previous_config = iface_param.re_search_children ('')
-            previous_config_list = []
-            for param in previous_config:
-                previous_config_list.append (param.text)
-            output_dictionary[hostname][iface_name]["previous_config"] = previous_config_list
+            #previous_config = iface_param.re_search_children ('')
+            #previous_config_list = []
+            #for param in previous_config:
+            #    previous_config_list.append (param.text)
+            #output_dictionary[hostname][iface_name]["previous_config"] = previous_config_list
+
+    # Get vlan list and
     return
 
 
@@ -139,8 +161,6 @@ def dict_to_xlsx(dictionary,out_file):
     :param dictionary: the name of the dictionary
     :param out_file: excel file
     """
-    import os
-    import xlsxwriter
     # Check if file exists and remove previous one
     if os.path.isfile(out_file):
         try:
@@ -151,7 +171,8 @@ def dict_to_xlsx(dictionary,out_file):
                     os.remove(out_file)
                     break
                 except PermissionError:
-                    input(f"Please close {os.path.basename(out_file)} Excel file and press enter")
+                    print(f"[ERROR] {os.path.basename(out_file)} is actually opened !!")
+                    input(f"Please close it and press enter.")
     # Create an excel workbook
     workbook = xlsxwriter.Workbook(out_file)
     worksheet = workbook.add_worksheet("Analyse")
@@ -182,8 +203,6 @@ def dict_to_xlsx(dictionary,out_file):
 
 if __name__ == "__main__":
     try:
-        import os
-        from tqdm import tqdm
         welcome_msg = "Create Excel file from CISCO cfg with one line per interface"
         welcome_len = len(welcome_msg) + 4
         print("*"*welcome_len)
@@ -194,14 +213,14 @@ if __name__ == "__main__":
             directory = input("Please type path where files are located:")
             if os.path.isdir(directory):
                 break
-        dict = {}
+        switch_params = dict()
         out_file = os.path.join(directory,"output.xlsx")
 
         # Main
-        file_list = search_file_in_directory(directory,filter="confg")
+        file_list = search_file_in_directory(directory,filter="config")
         for file in tqdm(file_list,desc="Analysing cisco Cfg file"):
-            sh_run_to_dict (file,dict,"ios")
-        dict_to_xlsx(dict,out_file)
+            sh_run_to_dict (file,switch_params,"nxos")
+        dict_to_xlsx(switch_params,out_file)
 
         end_msg = "Script is now finished, out file is named: "+os.path.basename(out_file)
         end_len = len(end_msg) + 4
